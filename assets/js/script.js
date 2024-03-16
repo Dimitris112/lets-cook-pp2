932b1dabb8b41cc75f782e4a379d0a2bb0b81c42
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-03-16 22:30:04 +0000

    Fixed the function to display recipes

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,24 +94,29 @@ document.addEventListener("DOMContentLoaded", function () {
     function displayRecipes(recipes) {
         const recipeContainer = document.getElementById("recipeContainer");
         recipeContainer.innerHTML = "";
-        recipes.forEach((recipe) => {
-            const recipeElement = document.createElement("div");
-            recipeElement.classList.add("recipe");
-            recipeElement.innerHTML = `
-            <h3>${recipe.strMeal}</h3>
-            <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}">
-            <button class="viewRecipeButton" data-recipe-id="${recipe.idMeal}">View Recipe</button>
-        `;
-            recipeContainer.appendChild(recipeElement);
-        });
 
-        const viewRecipeButtons = document.getElementsByClassName("viewRecipeButton");
-        viewRecipeButtons.forEach((button) => {
-            button.addEventListener("click", () => {
-                const recipeId = button.getAttribute("data-recipe-id");
-                fetchAndDisplayRecipeDetails(recipeId);
+        if (recipes && recipes.length > 0) {
+            recipes.forEach((recipe) => {
+                const recipeElement = document.createElement("div");
+                recipeElement.classList.add("recipe");
+                recipeElement.innerHTML = `
+                    <h3>${recipe.strMeal}</h3>
+                    <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}">
+                    <button class="viewRecipeButton" data-recipe-id="${recipe.idMeal}">View Recipe</button>
+                `;
+                recipeContainer.appendChild(recipeElement);
             });
-        });
+
+            const viewRecipeButtons = document.getElementsByClassName("viewRecipeButton");
+            for (let i = 0; i < viewRecipeButtons.length; i++) {
+                viewRecipeButtons[i].addEventListener("click", () => {
+                    const recipeId = viewRecipeButtons[i].getAttribute("data-recipe-id");
+                    fetchAndDisplayRecipeDetails(recipeId);
+                });
+            }
+        } else {
+            recipeContainer.innerHTML = "<p class='no-recipes-message'>No recipes found.</p>";
+        }
     }
 
     // Event listener to close the modal when the user clicks outside
